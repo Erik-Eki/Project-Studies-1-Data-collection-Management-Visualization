90831a969b8778bd0c0f4a5045cca39f5834b4a3
anttiisopahkala <anttiisopahkala@kamk.fi>
2020-03-18 08:58:13 +0200

    Docker test 1

---
+++ data/application/init.sql
@@ -0,0 +1,51 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema cart
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `cart` ;
+
+-- -----------------------------------------------------
+-- Schema cart
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `cart` DEFAULT CHARACTER SET utf8 ;
+USE `cart` ;
+
+-- -----------------------------------------------------
+-- Table `cart_data`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cart_data` ;
+
+CREATE TABLE IF NOT EXISTS `cart_data` (
+  `idcart` INT NOT NULL,
+  `time` VARCHAR(45) NOT NULL,
+  `x` FLOAT NOT NULL,
+  `y` FLOAT NOT NULL,
+  `z` FLOAT NOT NULL,
+  `signal_stength` FLOAT NOT NULL)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cart_data`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cart_data` ;
+
+CREATE TABLE IF NOT EXISTS `cart_data` (
+  `idcart` INT NOT NULL,
+  `time` VARCHAR(45) NOT NULL,
+  `x` FLOAT NOT NULL,
+  `y` FLOAT NOT NULL,
+  `z` FLOAT NOT NULL,
+  `signal_stength` FLOAT NOT NULL)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
